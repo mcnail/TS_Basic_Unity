8335f4a5e1192594dea624a5f20149fcf2e9a92c
mcnail <mc.nail84@gmail.com>
2025-08-13 17:23:56 +0900

    add singleton pattern

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -4,14 +4,24 @@ public class Coins : MonoBehaviour
 {
     public int coinScore = 10;
 
-    public ScoreManager scoreMng;
+    //public ScoreManager scoreMng;
+
+    public ParticleSystem ps;
+    private void Start()
+    {
+        //scoreMng = GameObject.Find("Managers").GetComponent<ScoreManager>();
+        ps.Stop();
+    }
+
     //플레이어가 부딛혔을때, 스코어매니저한테 점수를 증가한다
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            scoreMng.stageScore = scoreMng.stageScore + coinScore;
-            Destroy(gameObject);
+            //scoreMng.stageScore = scoreMng.stageScore + coinScore;
+            ScoreManager.Instance.UpdateText(coinScore);
+            ps.Play();
+            Destroy(gameObject, 0.5f);
         }
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,15 +6,36 @@ public class ScoreManager : MonoBehaviour
     public int stageScore = 0;
 
     public TMP_Text tmTex;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //싱글턴 패턴
+    private static ScoreManager _instance; //앱 안에서 유일한 자리를 만들어라
+    public static ScoreManager Instance; 
+
     void Start()
     {
+        if(_instance == null) //유일한 자리가 비어있다면
+        {
+            _instance = this; //유일한 한자리에 나를 앉혀줘!
+            DontDestroyOnLoad(gameObject);
+        }
+        else //비어있지 않다면
+        {
+            DestroyImmediate(this);
+        }
+        Instance = _instance;
+
         stageScore = 0;    
+    }
+
+    public void UpdateText(int score)
+    {
+        stageScore = stageScore + score;
+        tmTex.text = stageScore.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        tmTex.text = stageScore.ToString();
+        //tmTex.text = stageScore.ToString();
     }
 }
